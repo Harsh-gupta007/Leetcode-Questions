9599f9d50af8c85e7bea3cd8728112b5c7ce6414
Harsh-gupta007 <harsh.gupta2025@glbajajgroup.org>
2026-08-23 15:55:52 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (83.34%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.c
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.c
@@ -1,24 +1,18 @@
 int maxProduct(int n) {
-    int max=0,product,digit,arr[100],i=0;
-    while(n!=0){
-        digit=n%10;
-        arr[i]=digit;
-        n/=10;
-        i++;
-    }
+    int max1 = 0, max2 = 0;
+    
+    while(n != 0) {
+        int digit = n % 10;
+        n /= 10;
 
-    for(int j=0;j<i;j++){
-        
-        for(int k=j+1;k<i;k++){
-
-            product=arr[j]*arr[k];
-            
-            if(product>max){
-                max= product;
-            }
+        if(digit > max1) {
+            max2 = max1;
+            max1 = digit;
+        }
+        else if(digit > max2) {
+            max2 = digit;
         }
     }
 
-    return max;
-
+    return max1 * max2;
 }
