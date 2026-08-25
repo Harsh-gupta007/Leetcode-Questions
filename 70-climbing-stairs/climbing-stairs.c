b0f6a79d4fd87dde1b1b67fe1fa3614f21c1c2d3
Harsh-gupta007 <harsh.gupta2025@glbajajgroup.org>
2026-08-25 13:56:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (40.15%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.c
@@ -0,0 +1,20 @@
+int climbStairs(int n) {
+    if(n==1){
+        return 1;
+    }
+    if(n==2){
+        return 2;
+    }
+
+    int prev1=1;
+    int prev2=2;
+    int current;
+
+    for(int i=3;i<=n;i++){
+        current = prev1+ prev2;
+
+        prev1=prev2;
+        prev2=current;
+    }
+    return current;
+}
