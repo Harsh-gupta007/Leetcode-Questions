88d39a0fc6577478aba86b43b4b663a82e3d3468
Harsh-gupta007 <harsh.gupta2025@glbajajgroup.org>
2026-08-28 17:58:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (69.58%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.c
@@ -0,0 +1,19 @@
+int searchInsert(int* nums, int n, int target) {
+    int st=0, end = n - 1;
+    int mid=0;
+
+    while(st <= end){
+
+    mid = st + (end - st)/2; 
+
+    if(target == nums[mid]){
+        return mid;
+    }else if(target < nums[mid]){
+        end = mid - 1;
+    }else{
+        st = mid + 1;
+    }
+
+    } 
+    return st;
+}
